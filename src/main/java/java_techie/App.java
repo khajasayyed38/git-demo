ab010f0495ca550dca7080f35d428ed7042fbcd3
khajasayyed38 <92858743+khajasayyed38@users.noreply.github.com>
2024-04-16 00:03:12 +0530

    Update App.java

--- src/main/java/java_techie/App.java
+++ src/main/java/java_techie/App.java
@@ -5,7 +5,10 @@ public class App {
     public void addData(String data){
         if (data.equals(Constant.DATA_TYPE)) {
             System.out.println("welcome");
-        }
+        }    
+    }
+    public void addedMethod(){
+        System.out.printlin("new method");
     }
     public static void main(String[] args) {
         System.out.println("Hello world");
